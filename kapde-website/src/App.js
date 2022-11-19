9a033f41ce3b5351f4d2065ed2c8762d56823fde
Parakh <parakhpawar623@gmail.com>
2022-11-20 00:36:46 +0530

    Routing_3

--- kapde-website/src/App.js
+++ kapde-website/src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
-import Mens from "./Components/Mens";
-import Women from "./Components/Women";
-import Kids from "./Components/Kids";
-import Login from "./Components/Login";
-import Your_orders from "./Components/Your_orders";
-import Cart from "./Components/Cart";
+import Mens from './Components/Mens';
+import Women from './Components/Women';
+import Kids from './Components/Kids';
+import Login from './Components/Login';
+import Your_orders from './Components/Your_orders';
+import Cart from './Components/Cart';
 
 function App() {
   return (
@@ -19,24 +19,24 @@ function App() {
         <Home />
 
         <Routes>
-          <Route exact path="/mens">
-            <Mens />
+          <Route exact path='/mens' element={<Mens />}>
+            
           </Route>
-          <Route exact path="/women">
-            <Women />
+          <Route exact path='/women' element={<Women />}>
+            
           </Route>
-          <Route exact path="/kids">
-            <Kids />
+          <Route exact path='/kids' element={<Kids />}>
+            
           </Route>
 
-          <Route exact path="/login">
-            <Login />
+          <Route exact path='/login' element={ <Login />}>
+           
           </Route>
-          <Route exact path="/your_contacts">
-            <Your_orders />
+          <Route exact path='/your_contacts' element={<Your_orders />}>
+            
           </Route>
-          <Route exact path="/cart">
-            <Cart />
+          <Route exact path='/cart' element={ <Cart />}>
+           
           </Route>
         </Routes>
       </Router>
